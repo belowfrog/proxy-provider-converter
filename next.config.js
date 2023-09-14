ff791e5cac694dfc54c05d11bcab3f0feb2f66d5
belowfrog <belowfrog@gmail.com>
2023-09-14 15:17:02 +0800

    feat: remove preact

--- next.config.js
+++ next.config.js
@@ -6,13 +6,14 @@ module.exports = withBundleAnalyzer({
   swcMinify: true,
   webpack(config, { dev, isServer }) {
     // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
+    // if (!dev && !isServer) {
+    //   Object.assign(config.resolve.alias, {
+    //     "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+    //     react: "preact/compat",
+    //     "react-dom/test-utils": "preact/test-utils",
+    //     "react-dom": "preact/compat",
+    //   });
+    // }
 
     return config;
   },
